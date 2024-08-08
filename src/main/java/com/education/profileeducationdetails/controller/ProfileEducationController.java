b43c7775e40e811a558465dc5cbcb04ce89565df
pratima-01-95-a <pratima.dhumal96@gmail.com>
2024-08-08 22:10:50 +0530

    commit

--- src/main/java/com/education/profileeducationdetails/controller/ProfileEducationController.java
+++ src/main/java/com/education/profileeducationdetails/controller/ProfileEducationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class ProfileController {
+public class ProfileEducationController {
 
 
     @Autowired
